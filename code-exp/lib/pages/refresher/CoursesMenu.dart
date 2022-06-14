d72c004f794ff56124efb17ccd88f7fa17d79574
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2022-06-14 18:17:43 +0800

    Add refresher course cards
    Added cards for refresher course

--- code-exp/lib/pages/refresher/CoursesMenu.dart
+++ code-exp/lib/pages/refresher/CoursesMenu.dart
@@ -1,4 +1,12 @@
-var list = <Course>[
+class Course {
+  final String name;
+  final String abbr;
+  final String page;
+
+  Course({required this.name, required this.abbr, required this.page});
+}
+
+var course = <Course>[
   Course(
     name: 'Basic CPR Course',
     abbr: 'CPR',
--- code-exp/lib/pages/refresher/refresherCourse.dart
+++ code-exp/lib/pages/refresher/refresherCourse.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'CoursesMenu.dart';
+
 class refresherCourseScreen extends StatefulWidget {
   const refresherCourseScreen({Key? key}) : super(key: key);
 
@@ -11,6 +13,55 @@ class _refresherCourseScreenState extends State<refresherCourseScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('NS Refresher Course')), body: Scaf);
+        appBar: AppBar(title: Text('NS Refresher Course')),
+        body: GridView.builder(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            itemCount: course.length,
+            itemBuilder: (context, index) {
+              var item = course[index];
+              return Card(
+                  elevation: 4.0,
+                  child: Stack(
+                    fit: StackFit.loose,
+                    alignment: Alignment.center,
+                    children: <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            item.abbr,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 40,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Text(
+                            item.name,
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          right: 8.0,
+                          bottom: 8.0,
+                        ),
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {});
+                              print(item.abbr + ' selected');
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ));
+            }));
   }
 }
