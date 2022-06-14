369220dc0c50ddb94ffa6fe5b31a6d65527dd183
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2022-06-14 18:28:28 +0800

    Change refresher padding
    lookin even better now!

--- code-exp/lib/pages/refresher/refresherCourse.dart
+++ code-exp/lib/pages/refresher/refresherCourse.dart
@@ -14,59 +14,66 @@ class _refresherCourseScreenState extends State<refresherCourseScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('NS Refresher Course')),
-        body: GridView.builder(
-            gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-            itemCount: course.length,
-            itemBuilder: (context, index) {
-              var item = course[index];
-              return Card(
-                  elevation: 4.0,
-                  child: Stack(
-                    fit: StackFit.loose,
-                    alignment: Alignment.center,
-                    children: <Widget>[
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            item.abbr,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 40,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 3,
+                crossAxisSpacing: 3,
+              ),
+              itemCount: course.length,
+              itemBuilder: (context, index) {
+                var item = course[index];
+                return Card(
+                    color: Color(0xA0C8E6C9),
+                    elevation: 1.0,
+                    child: Stack(
+                      fit: StackFit.loose,
+                      alignment: Alignment.center,
+                      children: <Widget>[
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             Text(
-                              item.name,
+                              item.abbr,
                               textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 40,
+                              ),
                             ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                          ]),
-                      //padding used for gesture
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          right: 8.0,
-                          bottom: 8.0,
+                          ],
                         ),
-                        child: Align(
-                          alignment: Alignment.bottomRight,
-                          child: GestureDetector(
-                            onTap: () {
-                              setState(() {});
-                              print(item.abbr + ' selected');
-                            },
+                        Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Text(
+                                item.name,
+                                textAlign: TextAlign.center,
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                            ]),
+                        //padding used for gesture
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            right: 8.0,
+                            bottom: 8.0,
+                          ),
+                          child: Align(
+                            alignment: Alignment.bottomRight,
+                            child: GestureDetector(
+                              onTap: () {
+                                setState(() {});
+                                print(item.abbr + ' selected');
+                              },
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ));
-            }));
+                      ],
+                    ));
+              }),
+        ));
   }
 }
