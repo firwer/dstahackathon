98146d313401f90b0c35aeb44e5b9e02ee55856e
YongbinWang <yongbin_01@hotmail.com>
2022-06-14 19:42:13 +0800

    Updated eCanteen

--- code-exp/lib/pages/CanteenCart.dart
+++ code-exp/lib/pages/CanteenCart.dart
@@ -17,14 +17,33 @@ class _CartState extends State<Cart> {
 
   @override
   Widget build(BuildContext context) {
+    double cost = 0;
+    for (var i = 0; i < _cart.length; i++) {
+      cost += _cart[i].price;
+      print(cost);
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text('Cart'),
+        actions: [
+          ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(Colors.orangeAccent)),
+            onPressed: (() => {}),
+            child: Text(
+              'Payment:' + ' \$' + cost.toString(),
+              style: TextStyle(
+                  color: Color(0XFF4d7753), fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
       ),
       body: ListView.builder(
           itemCount: _cart.length,
           itemBuilder: (context, index) {
             var item = _cart[index];
+
             return Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: 8.0, vertical: 2.0),
@@ -35,7 +54,7 @@ class _CartState extends State<Cart> {
                     item.icon,
                     color: item.color,
                   ),
-                  title: Text(item.name),
+                  title: Text(item.name + ' ' + '\$' + item.price.toString()),
                   trailing: GestureDetector(
                       child: Icon(
                         Icons.remove_circle,
