65d805556f00d80d37d466d1336123765cfd8c4f
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2022-06-14 17:58:05 +0800

    Add refresher route & screen
    Added a new route for refresher page & screen

---
+++ code-exp/lib/pages/refresher/CoursesMenu.dart
@@ -0,0 +1,22 @@
+var list = <Course>[
+  Course(
+    name: 'Basic CPR Course',
+    abbr: 'CPR',
+    page: '/courses/cpr',
+  ),
+  Course(
+    name: 'Soldier Fundementals',
+    abbr: 'SOFUN',
+    page: '/courses/sofun',
+  ),
+  Course(
+    name: 'Vehicle Commander Refresher',
+    abbr: 'VCR',
+    page: '/courses/vcr',
+  ),
+  Course(
+    name: 'Map Planning Exercise',
+    abbr: 'MAPEX',
+    page: '/courses/mapex',
+  ),
+];
---
+++ code-exp/lib/pages/refresher/refresherCourse.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class refresherCourseScreen extends StatefulWidget {
+  const refresherCourseScreen({Key? key}) : super(key: key);
+
+  @override
+  State<refresherCourseScreen> createState() => _refresherCourseScreenState();
+}
+
+class _refresherCourseScreenState extends State<refresherCourseScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('NS Refresher Course')), body: Scaf);
+  }
+}
